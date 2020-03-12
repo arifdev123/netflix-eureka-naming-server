6b40856316c418ffa9861c90e45cfb5c9fb4e01f
Mohamed Ismail Arif <mhdarif.vdm@gmail.com>
2020-03-12 13:05:28 +0530

    upto zuul api gateway implementation done

---
+++ src/test/java/co/in/javacoder/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.in.javacoder.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
